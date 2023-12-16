fa3fbd7d2747ca1f04afef5984efd948ec454d7b
Michael Hu <mbhu@umich.edu>
2023-12-15 20:52:00 -0500

    make file

--- getMikedEncryptor/main.cpp
+++ getMikedEncryptor/main.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 #include <fstream>
+#include <deque>
+#include <stack>
+#include "cryptlib.h"
+#include "rijndael.h"
+#include "modes.h"
+#include "files.h"
+#include "osrng.h"
+#include "hex.h"
 
 
 int main(int argv, char* argc[])
 {
     std::cout << "hello world\n";
+
+    
+    
+
     return 0;
 }
